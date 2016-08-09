d4b7f08a6919a4f1a2186bd5a2a467940ca2bf1d
Stephen Shanahan <stephenshanahan91@gmail.com>
2016-08-09 15:49:31 -0700

    make chromedriver universal

--- chromedriver.py
+++ chromedriver.py
@@ -1,6 +1,7 @@
+import os
 from selenium import webdriver
 
-path_to_chromedriver = '/home/sshanahan91/Downloads/AGDQ/chromedriver' # change path as needed
+path_to_chromedriver = os.path.dirname(os.path.abspath(__file__)) + '/chromedriver' # change path as needed
 browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
 url = 'https://gamesdonequick.com/tracker/'
