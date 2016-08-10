b1a582740a29465e94c774ef76a664837182111c
Stephen Shanahan <stephenshanahan91@gmail.com>
2016-08-10 14:46:38 -0700

    Add tag method based on description and title

--- chromedriver.py
+++ chromedriver.py
@@ -33,10 +33,14 @@ def get_runs_by_event(event = None):
 		print "youtube_link: " + "--added later--"
 
 		# split full runner text by comma, remove all white space
-		runners = [final.strip() for final in run[1].text.encode('utf-8').split(',')]
+		runners = get_runners(run[1])
+		if runners:
 		# show list as a string seperated by slashes.
-		print "runners:      " + "/".join(runners)
+			print "runners:      " + "/".join(runners)
 		print "description:  " + run[2].text.encode('utf-8').strip()
+		tags = get_tags(run[2], run[0])
+		if tags:
+			print "tags:         " + "/".join(tags)
 		print ""
 
 
@@ -60,6 +64,62 @@ def get_all_events():
 		print "event_name: " + run.get_attribute("innerHTML").strip()
 		print ""
 
+def get_runners(runners_webelem):
+	return [final.strip() for final in runners_webelem.text.encode('utf-8').split(',')]
+
+def get_tags(description, title):
+	text = description.text.encode('utf-8').strip().lower() + title.text.encode('utf-8').strip().lower()
+	tag_list = []
+
+	if 'boss' in text:
+		tag_list.append('Boss Mode')
+
+	if ('any%' in text) or ('any %' in text):
+		tag_list.append('Any%')
+
+	if ('low%' in text) or ('low %' in text):
+		tag_list.append('Low%')
+
+	if ('100%' in text) or ('100 %' in text):
+		tag_list.append('100%')
+
+	if 'race' in text:
+		tag_list.append('Race')
+
+	if ('2p' in text) or ('2 player' in text) or ('2 way' in text):
+		tag_list.append('2p')
+
+	if ('4p' in text) or ('4 player' in text) or ('4 way' in text):
+		tag_list.append('4p')
+
+	if ('8p' in text) or ('8 player' in text) or ('8 way' in text):
+		tag_list.append('8p')
+
+	if ('ng+' in text) or ('new game+' in text) or ('new game +' in text):
+		tag_list.append('New Game+')
+
+	if ('blindfold' in text):
+		tag_list.append('Blindfolded')
+
+	if 'good end' in text:
+		tag_list.append('Good Ending')
+
+	if 'bad end' in text:
+		tag_list.append('Bad Ending')
+
+	if 'glitch' in text:
+		tag_list.append('Glitched')
+
+	if ('coop' in text) or ('co-op' in text):
+		tag_list.append('4p')
+
+	if 'hard mode' in text:
+		tag_list.append('Hard Mode')
+
+	if 'best end' in text:
+		tag_list.append('Best Ending')
+
+	return tag_list
 
 get_runs_by_event('agdq2016')
 browser.close()
