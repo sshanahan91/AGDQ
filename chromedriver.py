1ffd2d329df7c1d9a12d6b65c99c9c6d8001f271
Stephen Shanahan <stephenshanahan91@gmail.com>
2016-08-10 12:25:08 -0700

    get all events data. including link id information

--- chromedriver.py
+++ chromedriver.py
@@ -7,9 +7,10 @@ browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 url = 'https://gamesdonequick.com/tracker/runs'
 browser.get(url)
 
-all_objects = browser.find_elements_by_tag_name('tr')
+# Runs
+all_runs = browser.find_elements_by_tag_name('tr')
 
-for i in range(1, len(all_objects)):
+for i in range(1, len(all_runs)):
 	run = browser.find_elements_by_xpath('//table/tbody/tr[%d]/td' % i)
 
 	print "Title:       " + run[0].text.encode('utf-8').strip()
@@ -18,5 +19,19 @@ for i in range(1, len(all_objects)):
 	print "started_at:  " + run[3].text.encode('utf-8').strip()
 	print "ended_at:    " + run[4].text.encode('utf-8').strip()
 	print "bid_war:     " + run[5].text.encode('utf-8').strip()
-	print "\n"
+	print ""
+
+
+# Events
+all_events = browser.find_elements_by_xpath('//*[@id="bs-example-navbar-collapse-1"]/ul/li[7]/ul/li')
+
+for i in range(1, len(all_events)-1):
+	run_link = browser.find_element_by_xpath('//*[@id="bs-example-navbar-collapse-1"]/ul/li[7]/ul/li[%d]/a' % i)
+	run = browser.find_element_by_xpath('//*[@id="bs-example-navbar-collapse-1"]/ul/li[7]/ul/li[%d]/a' % i)
+	link = run_link.get_attribute("href").split("/")
+	
+	print "event_id:   " + link[len(link)-1]
+	print "event_name: " + run.get_attribute("innerHTML").strip()
+	print ""
+
 browser.close()
