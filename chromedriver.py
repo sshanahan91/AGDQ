7466192725ad58675ae425463fce29e263ab8ccc
Stephen Shanahan <stephenshanahan91@gmail.com>
2016-08-10 11:51:20 -0700

    Fix ASCII error, /runs works for text data

--- chromedriver.py
+++ chromedriver.py
@@ -12,11 +12,11 @@ all_objects = browser.find_elements_by_tag_name('tr')
 for i in range(1, len(all_objects)):
 	run = browser.find_elements_by_xpath('//table/tbody/tr[%d]/td' % i)
 
-	print "Title:       " + run[0].text
-	print "Players:     " + run[1].text
-	print "Description: " + run[2].text
-	print "started_at:  " + run[3].text
-	print "ended_at:    " + run[4].text
-	print "bid_war:     " + run[5].text
+	print "Title:       " + run[0].text.encode('utf-8').strip()
+	print "Players:     " + run[1].text.encode('utf-8').strip()
+	print "Description: " + run[2].text.encode('utf-8').strip()
+	print "started_at:  " + run[3].text.encode('utf-8').strip()
+	print "ended_at:    " + run[4].text.encode('utf-8').strip()
+	print "bid_war:     " + run[5].text.encode('utf-8').strip()
 	print "\n"
 browser.close()
