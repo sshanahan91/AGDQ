6ceff4d6be6f85302aa2041a2bed032a9ba0597d
Stephen Shanahan <stephenshanahan91@gmail.com>
2016-10-08 18:40:59 -0700

    link /events to database data, clean templates

--- events/urls.py
+++ events/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
 
     #event/{event_id}
-    url(r'event/(?P<event_id>[a-z]{1,10}[0-9]{0,4})$', views.detail_id, name='event_detail_id'),
+    url(r'event/(?P<event_id>[a-z]{1,10}[0-9]{0,4})$', views.detail, name='event_detail_id'),
 
     #event/{event_p_id}
-    url(r'event/(?P<event_id>[0-9]+)$', views.detail, name='event_detail'),
+    url(r'event/(?P<event_id>[0-9]+)$', views.detail_id, name='event_detail'),
 
 	#/tracker (index)
     url(r'', views.index, name='index'),
--- events/views.py
+++ events/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from .models import Event
+from .models import Event
 
 # Create your views here.
 def index(request):
@@ -10,14 +10,16 @@ def index(request):
 	return render(request, 'events/index.html', context)
 
 def detail(request, event_id):
+	event = Event.objects.get(event_id__startswith=event_id)
 	context = {
-		'event_id': event_id
+		"event": event,
 	}
 	return render(request, 'events/details.html', context)
 
 
 def detail_id(request, event_id):
+	event = Event.objects.get(id=event_id)	
 	context = {
-		'event_id': event_id
+		'event': event,
 	}
 	return render(request, 'events/details.html', context)
