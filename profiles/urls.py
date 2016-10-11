0a6211e1e8c43b29f4b6962075256d24d638a2b3
Stephen Shanahan <stephenshanahan91@gmail.com>
2016-10-10 18:12:29 -0700

    fix the / bug in the URLs

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
 
     #donor/{donor_id}
-    url(r'/(?P<donor_id>[0-9]+)$', views.detail, name='donor_detail'),
+    url(r'(/){1}(?P<donor_id>[0-9]+)', views.detail, name='donor_detail'),
 
 	#donors (index)
-    url(r's', views.index, name='index'),
+    url(r's[/]?$', views.index, name='index'),
 
 ]
