e1d47197fbc3187bde48a6f849fc2d84bcaed93c
Stephen Shanahan <stephenshanahan91@gmail.com>
2016-10-10 18:23:39 -0700

    Add User profiles to database to prepare for scraping

--- AGDQ/settings.py
+++ AGDQ/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'events.apps.EventsConfig',
+    'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- profiles/models.py
+++ profiles/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+	user_id    = models.CharField(max_length = 15)
+	name       = models.CharField(max_length = 150)
+	alias      = models.CharField(max_length = 150)
